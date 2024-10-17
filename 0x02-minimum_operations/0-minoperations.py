6a683f641f4eff4489032c5a680300925785d819
ZiadAS40 <zead4036@gmail.com>
2024-10-17 22:29:59 +0300

    add the function file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!usr/bin/python3
+"""make the min operations function"""
+
+
+def minOperations(n):
+    """
+    finding the minimum number of opertions in a file
+    """
+
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
