510e4547d48c5c7514bb91dfa3fcc60e2c3325e4
ZiadAS40 <zead4036@gmail.com>
2024-11-08 04:04:03 +0200

    adding the final file

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import sys
+
+
+def is_valid(board, row, col):
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
+
+
+def solve_nqueens(N):
+    def backtrack(row):
+        if row == N:
+            solution = [[i, board[i]] for i in range(N)]
+            solutions.append(solution)
+            return
+        for col in range(N):
+            if is_valid(board, row, col):
+                board[row] = col
+                backtrack(row + 1)
+                board[row] = -1
+
+    solutions = []
+    board = [-1] * N
+    backtrack(0)
+    return solutions
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solutions = solve_nqueens(N)
+    for solution in solutions:
+        print(solution)
+
+
+if __name__ == "__main__":
+    main()
