07d209440e36cb31d7cfb74d6da5e3b43382c36a
ZiadAS40 <ziad403612@gmail.com>
2025-05-02 18:52:41 +0300

    second try of min OPs problem

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,22 +3,31 @@
 
 
 def minOperations(n):
-    """
-    finding the minimum number of opertions in a file
-    """
-
-    if n <= 1:
+    if n < 2:
         return 0
 
-    operations = 0
-    factor = 2
+    def get_primes(n):
+        primes = []
+        for num in range(2, n):
+            is_prime = True
+            for i in range(2, int(num**0.5)+1):
+                if num % i == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                primes.append(num)
+        return primes
 
-    while n > 1:
+    primes = get_primes(int(n**0.5))
 
-        while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
+    def analyze(n, primes):
+        result = 0
+        for prime in primes:
+            division = n / prime
+            if division.is_integer() and prime < n:
+                result += prime
+                result += analyze(int(division), primes)
+                return result
+        return n
 
-    return operations
-    
+    return analyze(n, primes)
